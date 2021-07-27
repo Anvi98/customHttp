14c2d313ffc7b598d8f584e6100c4e5242b507f9
Anvi Alex <epononanvialex@gmail.com>
2021-07-27 21:51:56 +0300

    Add POST and PUT method

--- easyHttp.js
+++ easyHttp.js
@@ -17,8 +17,38 @@ easyHttp.prototype.get = function(url, callback) {
 
   this.http.send();
 }
+
 // Make an HTTP POST Request
+easyHttp.prototype.post = function(url, data, callback){
+  
+  this.http.open('POST', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function(){
+      callback(null, self.http.responseText);
+  }
+
+
+  this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP PUT Request
+
+easyHttp.prototype.put = function(url, data, callback){
+  
+  this.http.open('PUT', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function(){
+      callback(null, self.http.responseText);
+  }
+
+
+  this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP DELETE Request
